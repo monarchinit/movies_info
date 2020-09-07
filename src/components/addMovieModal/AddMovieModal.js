fb300c013beeabae85a404ac290169bee16eabbf
Yaroslav Lototskyi <lotslav@gmail.com>
2020-09-07 18:13:13 +0300

    fix 2 3 7.  work with validation

--- src/components/addMovieModal/AddMovieModal.js
+++ src/components/addMovieModal/AddMovieModal.js
@@ -1,11 +1,41 @@
 import React, { useState } from "react";
-import { Button, Select, TextField, InputLabel } from "@material-ui/core";
+import {
+  Button,
+  Select,
+  TextField,
+  InputLabel,
+  withStyles,
+} from "@material-ui/core";
 import css from "./AddMovieModal.module.css";
 import AddButton from "../addButton/AddButton";
 import { getFormData } from "../../helpers/helpers";
 import { connect } from "react-redux";
 import { addMovie } from "../../redux/operations";
 import mammoth from "mammoth";
+import Tips from "../tips/Tips";
+
+const ValidationTextField = withStyles({
+  root: {
+    marginBottom: "10px",
+    "& input:invalid + fieldset": {
+      borderColor: "red",
+    },
+    "& input:invalid:focus + fieldset": {
+      borderColor: "red",
+    },
+  },
+})(TextField);
+
+const ValidationSelectField = withStyles({
+  root: {
+    "& select:invalid ~ fieldset": {
+      borderColor: "red",
+    },
+    "& input:invalid:focus ~ fieldset": {
+      borderColor: "red",
+    },
+  },
+})(Select);
 
 const AddMovieModal = ({ setAddModalOpen, addMovie }) => {
   const [docTab, setDocTab] = useState(false);
@@ -16,13 +46,28 @@ const AddMovieModal = ({ setAddModalOpen, addMovie }) => {
         <AddButton
           clickCallback={() => setIdStars((s) => [...s, getArrayStars()[0]])}
         ></AddButton>
-        <TextField style={{ flex: "auto" }} required id="star" label="Star" />
+        <ValidationTextField
+          type="text"
+          inputProps={{ pattern: "^[A-Za-zА-Яа-яЁё ]*$" }}
+          style={{ flex: "auto" }}
+          required
+          variant="outlined"
+          id="star"
+          label="Star"
+          className={css.formItem}
+        />
+        <Tips>
+          <div className="tooltip">
+            <h2 className="popper">The "Star" field should only be letters!</h2>
+          </div>
+        </Tips>
       </div>,
     ];
   }
 
   const onHandleSubmit = (e) => {
     e.preventDefault();
+    console.log(e.target, "e.target");
     const result = getFormData(e.target);
     addMovie(result, setAddModalOpen);
   };
@@ -84,8 +129,10 @@ const AddMovieModal = ({ setAddModalOpen, addMovie }) => {
       className="containerModal"
     >
       <div className="wrapperModal">
-        <h2>You can add a movie or DOCUMENT with movies here!</h2>
-        <div>
+        <h2 className={css.title}>
+          You can add a movie or DOCUMENT with movies here!
+        </h2>
+        <div className={css.tabsWrapper}>
           <span
             className={css.tabs}
             style={{ color: docTab ? "gray" : "black" }}
@@ -107,23 +154,39 @@ const AddMovieModal = ({ setAddModalOpen, addMovie }) => {
             style={{ display: "flex", flexDirection: "column" }}
             onSubmit={onHandleSubmit}
           >
-            <TextField required id="title" label="Title" />
-            <TextField type="number" required id="year" label="Release Year" />
-            <div style={{ marginBottom: "10px" }}></div>
+            <ValidationTextField
+              error={false}
+              required
+              variant="outlined"
+              id="title"
+              label="Title"
+              className={css.formItem}
+            />
+            <ValidationTextField
+              type="number"
+              inputProps={{ max: "2021", min: "1895" }}
+              required
+              variant="outlined"
+              id="year"
+              label="Release Year"
+              className={css.formItem}
+            />
             <InputLabel htmlFor="format">Format</InputLabel>
-            <Select
+            <ValidationSelectField
               native
               required
+              variant="outlined"
+              size="small"
               inputProps={{
                 name: "format",
                 id: "format",
               }}
+              className={css.formItem}
             >
-              <option aria-label="None" value="" />
               <option value="DVD">DVD</option>
               <option value="VHS">VHS</option>
               <option value="Blu-Ray">Blu-Ray</option>
-            </Select>
+            </ValidationSelectField>
 
             {idStars.map((e, i) => (
               <div key={i}>{e}</div>
