f08fb0f9e8987f0a1c33dba4f9d9cb1880c21b62
Yaroslav Lototskyi <lotslav@gmail.com>
2020-09-07 19:01:16 +0300

    fix 5. color NavLink

--- src/components/mainItems/MainItems.js
+++ src/components/mainItems/MainItems.js
@@ -5,7 +5,9 @@ const MainItems = ({ renderMovies }) => (
   <ul>
     {renderMovies?.map((el) => (
       <li className="item" key={el._id}>
-        <NavLink to={`/${el._id}`}>{el.title}</NavLink>
+        <NavLink className="navLink" to={`/${el._id}`}>
+          {el.title}
+        </NavLink>
       </li>
     ))}
   </ul>
